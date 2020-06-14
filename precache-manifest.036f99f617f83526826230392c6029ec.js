20fec9714b1449fa752a1cc731d496eee5b10471
himanshu-bhatti-nagarro <himanshu.bhatti@nagarro.com>
2020-06-14 19:59:18 +0530

    Updates

---
+++ precache-manifest.036f99f617f83526826230392c6029ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5517b93db29aa2e8ebf96a47bc5371",
+    "url": "/react-assignment-new/index.html"
+  },
+  {
+    "revision": "b372bb05fb0345dd7eb7",
+    "url": "/react-assignment-new/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e7664d6ec243193437e",
+    "url": "/react-assignment-new/static/js/2.1e5afb32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-assignment-new/static/js/2.1e5afb32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b372bb05fb0345dd7eb7",
+    "url": "/react-assignment-new/static/js/main.cc56d5b7.chunk.js"
+  },
+  {
+    "revision": "5524d19a10af62f30aaa",
+    "url": "/react-assignment-new/static/js/runtime-main.3958731f.js"
+  }
+]);
